be504b436a88bf069670c67382da3bd614662657
sankarshandev98 <gsankar164@gmail.com>
2023-05-01 20:55:00 +0530

    update

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://note-backend-4cs2.onrender.com",
+  baseUrl: "https://note-backend-qmzg.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
