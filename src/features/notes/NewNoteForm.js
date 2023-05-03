c25dc873080fbbe485b07021e5d8bf8163bd0cdc
sankarshandev98 <gsankar164@gmail.com>
2023-05-03 19:35:56 +0530

    update

--- src/features/notes/NewNoteForm.js
+++ src/features/notes/NewNoteForm.js
@@ -28,8 +28,7 @@ const NewNoteForm = ({ users }) => {
 
   const canSave = [title, text, userId].every(Boolean) && !isLoading;
 
-  const onSaveNoteClicked = async (e) => {
-    e.preventDefault();
+  const onSaveNoteClicked = async () => {
     if (canSave) {
       await addNewNote({ user: userId, title, text });
     }
@@ -51,56 +50,69 @@ const NewNoteForm = ({ users }) => {
   const content = (
     <>
       <p className={errClass}>{error?.data?.message}</p>
+      <div className="border 2 flex flex-col m-auto gap-4 items-center justify-between w-11/12 shadow-md border-blue-300 rounded-lg">
+        <h2 className="text-center py-4 font-sans text-3xl">
+          Create a New Note
+        </h2>
 
-      <form className="form" onSubmit={onSaveNoteClicked}>
-        <div className="form__title-row">
-          <h2>New Note</h2>
-          <div className="form__action-buttons">
-            <button className="icon-button" title="Save" disabled={!canSave}>
-              <MdSave size="40px" />
-            </button>
-          </div>
-        </div>
-        <label className="form__label" htmlFor="title">
-          Title:
-        </label>
-        <input
-          className={`form__input ${validTitleClass}`}
-          id="title"
-          name="title"
-          type="text"
-          autoComplete="off"
-          value={title}
-          onChange={onTitleChanged}
-        />
-
-        <label className="form__label" htmlFor="text">
-          Text:
-        </label>
-        <textarea
-          className={`form__input form__input--text ${validTextClass}`}
-          id="text"
-          name="text"
-          value={text}
-          onChange={onTextChanged}
-        />
-
-        <label
-          className="form__label form__checkbox-container"
-          htmlFor="username"
-        >
-          ASSIGNED TO:
-        </label>
-        <select
-          id="username"
-          name="username"
-          className="form__select"
-          value={userId}
-          onChange={onUserIdChanged}
-        >
-          {options}
-        </select>
-      </form>
+        <section className="flex">
+          <label className="text-center font-sans text-2xl" htmlFor="title">
+            Title:
+          </label>
+          <input
+            className={
+              `form__input ${validTitleClass}` &&
+              "rounded-lg border-2 border-blue-300"
+            }
+            id="title"
+            name="title"
+            type="text"
+            autoComplete="off"
+            value={title}
+            onChange={onTitleChanged}
+          />
+        </section>
+        <section className="py-4 flex items-center justify-between">
+          <label className="text-center font-sans text-2xl" htmlFor="text">
+            Text:
+          </label>
+          <textarea
+            className={
+              `form__input form__input--text ${validTextClass}` &&
+              "rounded-lg border-2 border-blue-300 ml-1"
+            }
+            id="text"
+            name="text"
+            value={text}
+            onChange={onTextChanged}
+          />
+        </section>
+        <section className="pb-6 flex items-center w-full justify-around">
+          <label
+            className="text-center pl-2 font-sans text-xl"
+            htmlFor="username"
+          >
+            ASSIGNED TO:
+          </label>
+          <select
+            id="username"
+            name="username"
+            className="py-2 px-4 border-blue-300 border rounded-md"
+            value={userId}
+            onChange={onUserIdChanged}
+          >
+            {options}
+          </select>
+        </section>
+      </div>
+      <button
+        className="icon-button border-2 flex m-auto items-center py-1 px-10 mt-5 rounded-md border-blue-300"
+        title="Save"
+        disabled={!canSave}
+        onClick={onSaveNoteClicked}
+      >
+        <MdSave size="40px" /> <p>Save It !</p>
+      </button>
     </>
   );
 
